85ac5a9341a6639c67d3ddef6fe0ddbcff1bfebb
Eric Ruhi <ericruhi96Agmail.com>
2023-02-24 14:00:39 -0800

    prints 10 times the numbers, from 0 to 14, followed by a new line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,14 @@
 void more_numbers(void)
 {
 	int count, j;
+
 	for (count = 0; count <= 9; count++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 			{
-				_putchar((j/10) + '0');
+				_putchar((j / 10) + '0');
 			}
 			_putchar((j % 10) + '0');
 		}
